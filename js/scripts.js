b8412ea8c5755277ac30f12f140a8c367281145c
Chris Finney and Ryan McAlpin <chris@>
2017-03-01 10:43:11 -0800

    add alert on a roll of 1, other roll results get added to turnScore

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
+var turnScore = 0;
+
 $(document).ready(function() {
   $("#player-one-roll-butt").click(function() {
-    $("#player-one-roll-result").text(Math.floor(Math.random() * 6) + 1);
+    var rollResult = Math.floor(Math.random() * 6) + 1;
+    $("#player-one-roll-result").text(rollResult);
+
+    if (rollResult === 1) {
+      alert("Rolled a 1!");
+    } else {
+      turnScore += rollResult;
+    }
+    console.log(turnScore);
   });
 });
