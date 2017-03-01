d50820691027a7797ae35b8558efbeca8c25298e
Chris Finney and Ryan McAlpin <ryan@>
2017-03-01 11:24:13 -0800

    add user names prompt on page load

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
+//Business Logic
+
 var turnScore = 0;
 var playerOneTotal = 0;
 
+function Player(inputName, score) {
+  this.userName = inputName;
+  this.score = score;
+}
+
+var Player1 = new Player(prompt("Player 1, enter your name."), 0);
+var Player2 = new Player(prompt("Player 2, enter your name."), 0);
 
 $(document).ready(function() {
+  $("#player-one-name").text(Player1.userName);
+  $("#player-two-name").text(Player2.userName);
+
   $("#player-one-roll-butt").click(function() {
     var rollResult = Math.floor(Math.random() * 6) + 1;
     $("#player-one-roll-result").text(rollResult);
@@ -18,7 +30,7 @@ $(document).ready(function() {
 
   $("#player-one-hold-butt").click(function() {
     playerOneTotal += turnScore;
-    // $("#player-one-total").text()
+    // $("#player-one-total").text(playerOneTotal);
     console.log(playerOneTotal);
   });
 
